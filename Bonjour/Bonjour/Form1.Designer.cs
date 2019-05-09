a72d22e0896a98a08a0edc193c8eb894770d2ecc
GwenaelWest <48352232+GwenaelWest@users.noreply.github.com>
2019-05-09 15:52:59 +0200

    Bonjour

---
+++ Bonjour/Bonjour/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Bonjour
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.cmdValider = new System.Windows.Forms.Button();
+            this.txt2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Votre nom";
+            // 
+            // txt1
+            // 
+            this.txt1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.Location = new System.Drawing.Point(126, 31);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(149, 30);
+            this.txt1.TabIndex = 1;
+            this.txt1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // cmdValider
+            // 
+            this.cmdValider.Enabled = false;
+            this.cmdValider.Location = new System.Drawing.Point(143, 144);
+            this.cmdValider.Name = "cmdValider";
+            this.cmdValider.Size = new System.Drawing.Size(116, 42);
+            this.cmdValider.TabIndex = 2;
+            this.cmdValider.Text = "Bienvenue";
+            this.cmdValider.UseVisualStyleBackColor = true;
+            this.cmdValider.Click += new System.EventHandler(this.cmdValider_Click);
+            // 
+            // txt2
+            // 
+            this.txt2.AutoSize = true;
+            this.txt2.Font = new System.Drawing.Font("Comic Sans MS", 12F);
+            this.txt2.Location = new System.Drawing.Point(34, 91);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(22, 23);
+            this.txt2.TabIndex = 3;
+            this.txt2.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 346);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.cmdValider);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Bonjour Qui ?";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Button cmdValider;
+        private System.Windows.Forms.Label txt2;
+    }
+}
+
---
+++ Bonjour/Bonjour/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Auteur: Gwenaël West - Date: 09.05.2019 - 
+//Exercice "Bonjour Qui ?" ICT-404
+namespace Bonjour
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdValider_Click(object sender, EventArgs e)
+        {
+            txt2.Text = ("Bonjour " + txt1.Text);
+            // Appel la méthôôôôde quand on click sur le btn valider
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            cmdValider.Enabled = true;
+            //méthode qui quand le nom est rentré le btn devient disponible
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
